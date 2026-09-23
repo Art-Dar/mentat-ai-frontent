a2c11ebd56c4b84f560a3a601ee1db3b0ed56ff7
Artem Verbytskyi <116598786+Ke11nyk@users.noreply.github.com>
2026-09-23 22:52:43 +0300

    feat: add icons to manifest.config.ts

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -19,4 +19,10 @@ export default defineManifest({
   ],
   permissions: ['storage', 'activeTab', 'contextMenus', 'scripting'],
   host_permissions: ['<all_urls>'],
+  icons: {
+    16: 'public/icons/icon16.png',
+    32: 'public/icons/icon32.png',
+    48: 'public/icons/icon48.png',
+    128: 'public/icons/icon128.png',
+},
 })
